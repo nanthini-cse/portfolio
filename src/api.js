a9be716bba89d4feade8c949a7043e0ebc1ae245
Nanthini <snanthinicse030@gmail.com>
2025-07-13 19:43:15 +0530

    Add production build and updated packages

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({ baseURL: 'http://localhost:3000/api' });
 
 export const fetchProjects = () => API.get('/projects');
 export const sendMessage = (formData) => API.post('/contact', formData);
